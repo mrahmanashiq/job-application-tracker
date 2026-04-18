4b47fc1b7f118aad23a5761967fdefd859011a52
mrahmanashiq <mizanur.r.ashiq@gmail.com>
2026-04-18 09:12:44 +0600

    Track favicon.svg; drop Gatsby public/ ignore rule

--- client/pages/_document.tsx
+++ client/pages/_document.tsx
@@ -4,6 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
         <link
           href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap"
           rel="stylesheet"
